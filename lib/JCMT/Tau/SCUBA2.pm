6db6fc527bf5c4744d2680c48546290487e3bae7
Tim Jenness <t.jenness@jach.hawaii.edu>
2010-06-03 13:24:48 -1000

    Add SCUBA-2 support
    JCMT::Tau::SCUBA2 includes the SCUBA-2 filter relationships.

---
+++ lib/JCMT/Tau/SCUBA2.pm
@@ -0,0 +1,85 @@
+package JCMT::Tau::SCUBA2;
+
+=head1 NAME
+
+JCMT::Tau::SCUBA2 - Tau conversions for SCUBA-2 filters
+
+=head1 SYNOPSIS
+
+   use JCMT::Tau;
+
+   ($tau450, $status)   = get_tau('450', 'CSO', $csotau);
+   ($trans850, $status) = transmission($airmass, $tau850);
+   ($airmass, $status)  = airmass($elevation);
+
+=head1 DESCRIPTION
+
+SCUBA-2 variant of the JCMT::Tau module. The interface is identical
+to that of JCMT::Tau but supports the SCUBA-2 filter names and the
+SCUBA-2 filter tau relations.
+
+=cut
+
+use strict;
+use warnings;
+
+use JCMT::Tau ();
+
+use Carp;
+use vars qw($VERSION @ISA @EXPORT %Tau_Relation);
+
+$VERSION = "1.00";
+
+require Exporter;
+
+@ISA = qw(Exporter);
+@EXPORT = qw(transmission get_tau airmass %Tau_Relation);
+
+%Tau_Relation = (
+                 "CSO:450" => [ 19.04, -0.018 ],  # Original filter (JTD May 2010)
+                 "CSO:850" => [  5.36, -0.006 ],  # Original filter (JTD May 2010)
+);
+
+# Calculate inverse
+JCMT::Tau::_invert_relations( \%Tau_Relation );
+
+# Call the original implementation with the new relation
+sub get_tau {
+  JCMT::Tau::get_tau_with_relation( @_[0..3], \%Tau_Relation );
+}
+
+# Provide stubs to call the original versions
+*airmass = \&JCMT::Tau::airmass;
+*transmission = \&JCMT::Tau::transmission;
+
+=head1 SEE ALSO
+
+JCMT::Tau, JCMT::Tau::WVM.
+
+=head1 AUTHOR
+
+Tim Jenness E<lt>t.jenness@jach.hawaii.eduE<gt>
+
+Copyright 2010 Science and Technology Facilities Council.
+All Rights Reserved.
+
+=head1 LICENCE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 3 of
+the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+MA 02111-1307, USA
+
+=cut
+
+1;
---
+++ t/get_tau_s2.t
@@ -0,0 +1,45 @@
+# -*- cperl -*-
+use strict;
+use Test;
+BEGIN { plan tests => 9 }
+use JCMT::Tau::SCUBA2;
+
+# ================================================================
+#   Test JCMT::Tau::get_tau
+#
+#  These tests have to be changed if the ratios are changed
+#
+# ================================================================
+
+# === 1st Test: GET TAU bad parameters ===
+
+my ($this,$stat) = get_tau("850W",'CSO',.1);
+
+ok( $stat, -1);
+
+# Input == output
+
+($this, $stat) = get_tau('450','450', 0.85);
+ok($stat, -2);
+ok($this, 0.85);
+
+# Same with a space
+($this, $stat) = get_tau('CSO','cso ', 428.2);
+ok($stat, -2);
+ok($this, 428.2);
+
+# ===  GET TAU ===
+
+($this,$stat) = get_tau(450,'CSO',.05);
+$this = sprintf("%.1lf",$this);
+
+ok($stat, 0);
+ok($this, '0.6');
+
+
+# === Now invert
+($this, $stat) = get_tau('CSO', '450', $this);
+ok($stat, 0);
+$this = sprintf("%.2lf",$this);
+ok($this, 0.05);
+
